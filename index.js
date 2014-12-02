bbe74581a435cca2f8718aef998caa7d87bd5622
James R. Qualls <Qualls.James@gmail.com>
2014-12-02 18:02:17 -0500

    getting started on v2

--- index.js
+++ index.js
@@ -1,67 +1,188 @@
-var _        = require('underscore');
+'use strict';
+
+require('colors');
+
+var _        = require('lodash');
 var util     = require('util');
-var request  = require('request');
+var bluebird = require('bluebird');
+var request  = bluebird.promisify(require('request'));
+var DEV_URL  = process.env.DEV_URL;
 var NIX_ENV  = process.env.NIX_ENV;
-var DEV_MODE = NIX_ENV === 'dev';
-var DEV_URL  = 'http://localhost:3080';
+var DEV_MODE = NIX_ENV === 'development';
 var PRO_URL  = 'https://api.nutritionix.com';
 
-var formatArgs = function(args) {
-    return [util.format.apply(util.format, Array.prototype.slice.call(args))];
-};
-
-var stringify = function (o) {
+function stringify(o) {
     return JSON.stringify(o, null, 4);
-};
+}
 
-var safeParse = function (o) {
+function formatArgs(args) {
+    return [util.format.apply(util.format, Array.prototype.slice.call(args))];
+}
+
+function parseJsonSafely(o) {
     try {
         return JSON.parse(o);
     } catch (e) {
+        return o;
+    }
+}
 
-        if (e.message === 'Unexpected token o') {
-            return o
+function DebugLogger(enabled) {
+    return function debugLogger(){
+        if (enabled) {
+            return console.log.apply(console.log,
+                                     formatArgs(arguments));
+        } else {
+            return void 0;
         }
+    };
+}
 
-        return null
-    }
-};
+function ApiMap(clientOpts, url, log){
 
-// Expose
-module.exports = function (credentials, debug) {
-
-    debug = debug || false;
-
-    if (debug === true) {
-
-        console.debug = function(){
-            return console.log.apply(console.log, formatArgs(arguments));
-        };
-
-    } else {
-
-        console.debug = function() {
-            return void 0
-        };
-
-    }
-
-    console.debug('Debugging is enabled');
-
-    // Allow url switching for DEV
-    var url  = (DEV_MODE ? DEV_URL : PRO_URL);
-    var raw  = require('./lib/raw')(request, credentials, stringify, safeParse);
-    var v1_1 = require('./lib/v1_1.js')(raw, url, '/v1_1/');
-
-    var apiLib = {
-        v1_1: v1_1,
-        url: url,
-        raw: raw,
-        safeParse: safeParse,
-        stringify: stringify,
-        formatArgs: formatArgs
+    var credentials = {
+        header: {
+            'X-APP-ID': clientOpts.appId,
+            'X-APP-KEY': clientOpts.appKey
+        },
+        query: {
+            appId: clientOpts.appId,
+            appKey: clientOpts.appKey
+        }
     };
 
-    console.debug('API Library'.blue,'\n',apiLib);
-    return apiLib;
-};
+    var map = {
+        v2: {
+            search: {
+                method: 'POST',
+                url: url + '/v2/search',
+                headers: credentials.header,
+                json: {
+                    limit: 10,
+                    offset: 0,
+                    'search_nutrient':'calories'
+                }
+            },
+            'brand_search': {
+                method: 'GET',
+                url: url + '/v2/search/brands',
+                qs: {}
+            },
+            autocomplete: {
+                method: 'GET',
+                url: url + '/v2/autocomplete',
+                qs: {}
+            }
+        }
+    };
+
+    function findKeyDeep(paths, obj) {
+
+        if (!Array.isArray(paths)) {
+            paths = paths.split('.');
+        }
+
+        var key = paths.shift();
+        var containsKey = key in obj;
+
+        // exit
+        if (containsKey && paths.length === 0) {
+            return _.cloneDeep(obj[key]);
+        }
+
+        // recurse
+        if(containsKey && paths.length > 0){
+            return findKeyDeep(paths, obj[key]);
+        }
+
+        // assert
+        throw new Error('Can\'t find key: \'%s\'');
+    }
+
+    function getOptions(path, payload){
+        var opts = findKeyDeep(path, map);
+        _.assign(opts[payload.key], payload.opts);
+        return opts;
+    }
+
+    function ApiResponse(results, expectedStatus){
+
+        var resp = results[0];
+        var body = parseJsonSafely(results[1]);
+        var status = resp.statusCode;
+        var statusOk = +status === +expectedStatus;
+        this.status = status;
+        this.statusOk = statusOk;
+        this.body = body;
+        this.success = statusOk && body !== null;
+        this.request = _.pick(resp.request, ['method',
+                                       'headers', 'uri', 'body']);
+
+        if (this.request.body) {
+            this.request.body = parseJsonSafely(this.request
+                                                    .body.toString());
+        }
+    }
+
+    function ResponseHandler(expectedStatus) {
+        expectedStatus = expectedStatus || 200;
+
+        return function responseHandler(results){
+
+            var apiRes = new ApiResponse(results,
+                                         expectedStatus);
+
+            var logColor = apiRes.statusOk ? 'green'
+                                           : 'red';
+
+            log('API Status'[logColor], apiRes.status);
+            log('API Response'[logColor], stringify(apiRes));
+
+            if (!apiRes.success) {
+                throw apiRes;
+            }
+
+            return apiRes.body;
+        };
+    }
+
+    this.ApiRequest = function ApiRequest(path, payloadKey, expectedStatus){
+        return function apiRequest(apiOpts) {
+            return request(getOptions(path, {
+                key: payloadKey,
+                opts: apiOpts
+            })).then(new ResponseHandler(expectedStatus));
+        };
+    };
+}
+
+function Nutritionix(clientOpts) {
+
+    _.defaults(clientOpts, {
+        appId: process.env.NIX_APP_ID,
+        appKey: process.env.NIX_APP_ID,
+        debug: false,
+        version: 'v2'
+    });
+
+    var url  = DEV_MODE ? DEV_URL : PRO_URL;
+    var log  = new DebugLogger(clientOpts.debug);
+    var apiMap = new ApiMap(clientOpts, url, log);
+
+    log('Debugging is enabled');
+
+    return {
+        search: new apiMap.ApiRequest('v2.search', 'json'),
+        autocomplete: new apiMap.ApiRequest('v2.autocomplete', 'qs'),
+        'brand_search': new apiMap.ApiRequest('v2.brand_search', 'qs')
+    };
+}
+
+// Exposing
+// =================
+// new Nutritionix({
+//     appId: '',
+//     appKey: ''
+// });
+
+module.exports = Nutritionix;

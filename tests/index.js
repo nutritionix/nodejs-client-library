4dde77c7a5224550b6e2c5e98ef46098fbc645c7
James R. Qualls <Qualls.James@gmail.com>
2014-12-03 17:11:04 -0500

    more progress stage 2

--- tests/index.js
+++ tests/index.js
@@ -14,60 +14,27 @@ function logJson(o) {
 }
 
 function handleError(e) {
-    console.error(logJson(e));
+    logJson(e);
+    throw e;
 }
 
-
-
-
-// GET https://api.nutritionix.com/v1_1/item?upc=52200004265
-// nutritionix.v1_1.item({
-//     upc: 52200004265
-// }, function (err, item) {
-//     console.log('Item Callback', sNull(err).red, nutritionix.stringify(item).green);
-// });
-
-// // GET https://api.nutritionix.com/v1_1/brand/51db37c3176fe9790a8991f6
-// nutritionix.v1_1.brand({
-//     id: '51db37c3176fe9790a8991f6'
-// }, function (err, brand){
-//     console.log('Brand Callback', sNull(err).red, nutritionix.stringify(brand).green);
-// });
-
-// // GET https://api.nutritionix.com/v1_1/search/mcdonalds?results=0:1
-// nutritionix.v1_1.search.standard({
-//     phrase: 'mcdonalds',
-//     results: '0:1'
-// }, function (err, results){
-//     console.log('Standard Search Callback', sNull(err).red, nutritionix.stringify(results).green);
-// });
-
-// // POST https://api.nutritionix.com/v1_1/search -d DATA
-// nutritionix.v1_1.search.advanced({
-//     fields: ['item_name','brand_name'],
-//     query: 'mcdonalds',
-//     offset:0,
-//     limit:1
-// }, function (err, results){
-//     console.log('Advanced Search Callback', sNull(err).red, nutritionix.stringify(results).green);
-// });
 
 nutritionix.search({
     q:'just salad'
 })
 .then(function (searchResults){
-    logJson(searchResults);
-    return nutritionix.autocomplete({
-        q:'salad'
+    // logJson(searchResults);
+    return nutritionix.item({
+        id: searchResults.results[0].resource_id
     });
 })
-.then(function (autocompleteResults){
-    logJson(autocompleteResults);
-    return nutritionix.brand_search({
-        q: 'burger'
+.then(function (item){
+    // logJson(item);
+    return nutritionix.brand({
+        id: item.brand.id
     });
 })
-.then(function(brandSearchResults){
-    logJson(brandSearchResults);
+.then(function(brand){
+    logJson(brand);
 })
 .catch(handleError);
